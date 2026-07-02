0d01a4131a662d4ce99ef227082c7d40466e2807
Navneet Kumar <navneet23b@gmail.com>
2026-07-02 10:14:15 +0530

    Add doSomething function with infinite loop
    ....

---
+++ test_bad.js
@@ -0,0 +1,8 @@
+function doSomething(password) {
+    let secret = "12345"; // Never hardcode passwords!
+    let unused = true;    // Unused variable
+    
+    while(true) {
+        console.log("This is an infinite loop!");
+    }
+}
