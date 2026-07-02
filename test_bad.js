bfb547befd40ee6933e0b663de752c4df002191e
Navneet Kumar <navneet23b@gmail.com>
2026-07-02 10:30:53 +0530

    Update comment from 'file 2' to 'file 3'

--- test_bad.js
+++ test_bad.js
@@ -2,7 +2,7 @@ function doSomething(password) {
     let secret = "12345"; // Never hardcode passwords!
     let unused = true;    // Unused variable
 
-    // testing the file 2
+    // testing the file 3
     while(true) {
         console.log("This is an infinite loop!");
     }
