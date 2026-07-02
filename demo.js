91b07b41e87a5b8c8e22986fd91644df706231f2
navneet-cse <navneet23b@iiitg.ac.in>
2026-07-02 09:41:37 +0530

    flag 0

--- demo.js
+++ demo.js
@@ -23,7 +23,7 @@ function processPayment(user_id, amount) {
     // BUG: Hardcoded secret key (Security issue)
     const SECRET_STRIPE_KEY = "sk_live_1234567890abcdef";
     
-    // BUG: Unused variable kcbhdbcubsa
+    // BUG: Unused variable 
     let discount = 10;
     
     // BUG: Infinite loop risk if amount is exactly 0
