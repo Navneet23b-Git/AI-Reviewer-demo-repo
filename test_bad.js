8fa82d52dc678e549c65fab220dd2d8b5490e729
Navneet Kumar <navneet23b@gmail.com>
2026-07-02 10:22:58 +0530

    Clean up doSomething function in test_bad.js
    Removed unused variable and commented out code.

--- test_bad.js
+++ test_bad.js
@@ -1,7 +1,7 @@
 function doSomething(password) {
     let secret = "12345"; // Never hardcode passwords!
     let unused = true;    // Unused variable
-    
+    //dfsdfdfd
     while(true) {
         console.log("This is an infinite loop!");
     }
